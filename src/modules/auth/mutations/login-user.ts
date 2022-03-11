5814afeb68820de80a7e87236ac697efb859859a
Solomon Eseme <kaperskyguru@gmail.com>
2022-03-11 21:54:20 +0100

    fix: cookie same site issue

--- src/modules/auth/mutations/login-user.ts
+++ src/modules/auth/mutations/login-user.ts
@@ -50,9 +50,9 @@ export default async (
         // domain: 'contentre.io',
         expires: remember ? new Date(Date.now() + thirtyDays) : undefined,
         httpOnly: true,
-        sameSite: ['LOCAL', 'DEVELOP'].includes(environment.context)
-          ? 'None'
-          : true,
+        // sameSite: ['LOCAL', 'DEVELOP'].includes(environment.context)
+        //   ? 'None'
+        //   : true,
         secure: true
       }
     })
