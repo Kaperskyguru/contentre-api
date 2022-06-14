8d805df7d8daaf17f4ad70f31792fdf7efa794ac
Solomon Eseme <kaperskyguru@gmail.com>
2022-06-14 21:42:50 +0100

    feat: add hiring feature

---
+++ prisma/migrations/20220614204217_add_unique_constraint/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[subscriptionId]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[activeSubscriptionId]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- CreateIndex
+CREATE UNIQUE INDEX "User_subscriptionId_key" ON "User"("subscriptionId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_activeSubscriptionId_key" ON "User"("activeSubscriptionId");
